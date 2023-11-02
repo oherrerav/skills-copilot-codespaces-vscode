e69743ee206091cfe4c92ed67f0c6758a0f183e2
oherrerav <oherrerav@gmail.com>
2023-11-02 21:42:41 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  var skills = document.getElementById("skills");
+  var member = document.getElementById("member");
+  var skillsMember = document.getElementById("skills-member");
+  var memberMember = document.getElementById("member-member");
+  skills.style.display = "block";
+  member.style.display = "none";
+  skillsMember.style.display = "block";
+  memberMember.style.display = "none";
+}
